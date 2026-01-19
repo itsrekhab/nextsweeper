37330a8d4ffc637a67ec2b7b4d640813934a346d
itsrekhab <73596345+itsrekhab@users.noreply.github.com>
2026-01-19 14:12:43 +0500

    fix: update outdated tests in utils.test.ts

--- app/game/utils.test.ts
+++ app/game/utils.test.ts
@@ -1,47 +1,39 @@
-import { describe, expect, test } from "vitest";
-import { generateField } from "./utils";
+import { test as baseTest, describe, expect } from "vitest";
+import { createMineFieldGenerator } from "./utils";
+
+const seed = Date.now();
+
+const test = baseTest.extend({
+  gen1: createMineFieldGenerator({ height: 10, width: 10, mines: 10 }, seed),
+  gen2: createMineFieldGenerator({ height: 10, width: 10, mines: 10 }, seed),
+});
 
 describe("seeded generation is consistent", () => {
-  test("without first click", () => {
-    const field1 = generateField({ height: 10, width: 10, mines: 10 }, 1);
-    const field2 = generateField({ height: 10, width: 10, mines: 10 }, 1);
-    expect(field1).toEqual(field2);
+  test("without first click", ({ gen1, gen2 }) => {
+    expect(gen1.init()).toEqual(gen2.init());
   });
 
-  test("with first click", () => {
-    const seed = Date.now();
-    const field1 = generateField({ height: 10, width: 10, mines: 10 }, seed, {
+  test("with first click", ({ gen1, gen2 }) => {
+    const firstClick = {
       x: 0,
       y: 0,
-    });
-    console.log(
-      field1.reduce(
-        (acc, row) =>
-          acc + row.reduce((acc2, cell) => acc2 + Number(cell.isMine), ""),
-        "",
-      ),
+    };
+    gen1.init();
+    gen2.init();
+    expect(gen1.generate(firstClick.x, firstClick.y)).toEqual(
+      gen2.generate(firstClick.x, firstClick.y),
     );
-    const field2 = generateField({ height: 10, width: 10, mines: 10 }, seed, {
-      x: 0,
-      y: 0,
-    });
-    console.log(
-      field2.reduce(
-        (acc, row) =>
-          acc + row.reduce((acc2, cell) => acc2 + Number(cell.isMine), ""),
-        "",
-      ),
-    );
-    expect(field1).toEqual(field2);
   });
 });
 
-test("field has the right amount of mines", () => {
+test("field has the right amount of mines", ({ gen1 }) => {
   expect(
-    generateField({ height: 10, width: 10, mines: 10 }, 1).reduce(
-      (acc, row) =>
-        acc + row.reduce((acc2, cell) => acc2 + Number(cell.isMine), 0),
-      0,
-    ),
+    gen1
+      .init()
+      .reduce(
+        (acc, row) =>
+          acc + row.reduce((acc2, cell) => acc2 + Number(cell.isMine), 0),
+        0,
+      ),
   ).toBe(10);
 });
