acedf1867f395d9dd6893cf6ca1c0cd0b89c2b35
itsrekhab <73596345+itsrekhab@users.noreply.github.com>
2026-01-20 17:48:47 +0500

    style: add distinct CSS variables to Header and Footer

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 right-0 left-0 z-10 bg-background shadow-[0_2px_0_rgba(0,0,0,0.1)]">
+    <header className="fixed top-0 right-0 left-0 z-10 bg-header shadow-[0_2px_0_rgba(0,0,0,0.1)]">
       <nav className="xl:max-w-7xl mx-auto flex h-12 w-full justify-between gap-4 py-2">
         <Links />
         <div className="flex items-center gap-2">
