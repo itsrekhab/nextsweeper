33d6a006ed62d7e91e1a278e2f243e89647e5120
itsrekhab <73596345+itsrekhab@users.noreply.github.com>
2026-01-26 17:51:39 +0500

    fix: adjust color contrast for a11y

--- _components/Footer.tsx
+++ _components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="col-[content-box] row-3 bg-footer w-full h-(--footer-height) flex items-center p-4">
-      <p className="text-sm text-gray-500 leading-none">
+      <p className="text-sm text-gray-600 dark:text-gray-400 leading-none">
         &copy; {new Date().getFullYear()} itsrekhab
       </p>
     </footer>
